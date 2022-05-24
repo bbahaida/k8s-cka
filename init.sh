650d8d1286a79bdbb2b6d5e527f8cad4fd0a84c5
Brahim Bahaida <bbahieda@gmail.com>
2022-05-24 18:49:33 +0000

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+alias k='kubectl'
+alias kg='kubectl get'
+alias ke='kubectl edit'
+alias kd='kubectl describe'
+alias kdd='kubectl delete'
+alias kgp='kubectl get pods'
+alias kgd='kubectl get deployments'
+alias kns='kubens'
+alias kcx='kubectx'
+alias wkgp='watch kubectl get pod'
